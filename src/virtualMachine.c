364901df90a70b29ea704072940599240628043a
sthaarwin <sthaarwin123@gmail.com>
2025-01-07 20:41:27 +0545

    push pop and print XD

--- src/virtualMachine.c
+++ src/virtualMachine.c
@@ -14,7 +14,51 @@ typedef enum{
     HALT
 }VM_Instructions;
 
+typedef struct{
+    VM_Instructions InstructionType;
+    int operand; 
+}Instruction;
+
+Instruction program[] = {
+   {.InstructionType = PUSH, .operand = 10}, 
+   {.InstructionType = PRINT},
+   {.InstructionType = POP},
+};
+
+#define PROGRAM_SIZE (sizeof(program)/sizeof(program[0]))
+#define MAX_STACK_SIZE 1024
+
+int stack[MAX_STACK_SIZE];
+int stack_size = 0;
+
+void push(int value){
+    stack[stack_size++] = value;
+}
+
+int pop(){
+    return stack[--stack_size];
+}
+
+
 int main(){
-    printf("Hello World");
+    size_t ip = 0;
+    for(ip = 0; ip < PROGRAM_SIZE; ip++){
+        switch(program[ip].InstructionType){
+            case PUSH:
+                push(program[ip].operand); 
+                break;
+
+            case POP:
+                printf("POP %d\n", pop());
+                break;
+
+            case PRINT:
+                printf("PRINT\n");
+                break;
+            
+            default:
+                assert(0 && "Unknown Instruction");
+        }
+    }
     return 0;
 }
